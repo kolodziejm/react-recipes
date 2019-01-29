be740abe13dd889f31c99c3058266ad6536b99df
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2019-01-29 18:53:15 +0100

    added animations with react-pose

--- client/src/components/Recipe/RecipeItem.jsx
+++ client/src/components/Recipe/RecipeItem.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import posed from 'react-pose';
 
-const RecipeItem = ({ _id, imageUrl, name, category }) => (
-  <li
+const RecipeItem = posed.li({
+  shown: { opacity: 1 },
+  hidden: { opacity: 0 }
+});
+
+export default ({ _id, imageUrl, name, category }) => (
+  <RecipeItem
     style={{ background: `url(${imageUrl}) center center / cover no-repeat` }}
     className="card"
   >
@@ -10,7 +16,5 @@ const RecipeItem = ({ _id, imageUrl, name, category }) => (
     <div className="card-text">
       <Link to={`/recipes/${_id}`}><h4>{name}</h4></Link>
     </div>
-  </li>
+  </RecipeItem>
 );
-
-export default RecipeItem
